60e7e0c1c43260b51acf95d0c6229f77d33a13be
pranit ingole <pranitingole1@gmail.com>
2021-09-05 15:55:20 +0530

    mark one add

--- src/components/data/projectdata.js
+++ src/components/data/projectdata.js
@@ -3,21 +3,22 @@ export const ProjectData = [
   {
     show: true,
     id: '1',
-    project: "stock profit or loss",
-    github: "https://github.com/pranitcode/Stock-profit-loss-Mark-14-JS",
-    demo: "https://stock-profit-or-loss-mark14.netlify.app/",
-    about: "It site for check profit or lodd in stck,build in Html css js",
+    project: "Do you know me ?",
+    github: "https://github.com/pranitcode/DoYouKnowMe.git",
+    demo: "https://replit.com/@pranitcode/Do-you-know-me?embed=true",
+    about: "It's cli app build in nodejs.",
     date: "Aug 2021"
   },
   {
     show: true,
-    id: '1',
+    id: '2',
     project: "stock profit or loss",
     github: "https://github.com/pranitcode/Stock-profit-loss-Mark-14-JS",
     demo: "https://stock-profit-or-loss-mark14.netlify.app/",
-    about: "It site for check profit or lodd in stck,build in Html css js",
+    about: "Its site for check profit or loss in stocks,build in Html css js",
     date: "Aug 2021"
   },
+
   {
     show: false,
     id: '1',
